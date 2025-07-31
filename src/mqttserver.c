ffc3d0dd676cfeac6f7e7267997771afe79feda4
Michal Medvecky <miskomedvecky@gmail.com>
2025-07-31 23:04:00 +0200

    +debug

--- src/mqttserver.c
+++ src/mqttserver.c
@@ -275,6 +275,7 @@ process_incoming_data_from_client(struct connection *conn, char *fixed_header, i
 
 		if (conn->type == MQTT_PINGREQ || conn->type == MQTT_DISCONNECT) {
 			// for messages with empty remaining length
+			log_debug("got me some ping request");
 			conn->message_size = -1;
 			if (conn->type == MQTT_DISCONNECT) {
 				log_info("Client %s disconnecting.", conn->client_id);				
@@ -632,6 +633,7 @@ process_mqtt_message(struct connection *conn, struct connections *conns) {
 			free(publish);
 			break;
 		case MQTT_PINGREQ:
+			log_debug("got ping req");
 			outgoing_message = calloc(2, 1);
 			if (!outgoing_message)
 				err(1, "process mqtt msg calloc outgoing_message");
